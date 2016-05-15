d6b85023c82953f096446842bb450395ad5b0d35
sabotuer99 <sabotuer99@gmail.com>
2016-05-15 09:35:46 -0600

    bypassed code that was throwing NPE

--- InMemoryIdentity/Models/IdentityModels.cs
+++ InMemoryIdentity/Models/IdentityModels.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using InMemoryIdentity.StorageProvider;
+using System;
+using System.Diagnostics;
 
 namespace InMemoryIdentity.Models
 {
@@ -11,12 +13,30 @@ namespace InMemoryIdentity.Models
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            try
+            {
+                var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+                return userIdentity;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.StackTrace);
+
+                var claims = new UserClaimsTable(new InMemoryContext()).FindByUserId(this.Id).Claims;
+
+                var userIdentity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie, ClaimTypes.Name, null);
+                userIdentity.AddClaim(new Claim(ClaimTypes.NameIdentifier, this.Id));
+                userIdentity.AddClaim(new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider", DefaultAuthenticationTypes.ApplicationCookie));
+                userIdentity.AddClaim(new Claim(ClaimTypes.Name, this.UserName));
+                
+                return userIdentity;
+            }
 
             //var userIdentity = new UserClaimsTable(new InMemoryContext()).FindByUserId(this.Id);
             
             // Add custom user claims here
-            return userIdentity;
+            
         }
     }
 
