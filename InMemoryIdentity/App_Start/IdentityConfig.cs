10a8b4949ca6d3d23e216c94c02b8bd3593947d5
sabotuer99 <sabotuer99@gmail.com>
2016-05-21 19:27:32 -0600

    removed unnecessary cast

--- InMemoryIdentity/App_Start/IdentityConfig.cs
+++ InMemoryIdentity/App_Start/IdentityConfig.cs
@@ -48,7 +48,7 @@ namespace InMemoryIdentity
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>() as InMemoryContext));
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
